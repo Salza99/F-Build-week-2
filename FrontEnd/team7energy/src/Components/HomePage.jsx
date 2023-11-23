551352fd821c35f9709445b5074472eea022edb8
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-23 14:37:58 +0100

    update component for canvas

--- FrontEnd/team7energy/src/Components/HomePage.jsx
+++ FrontEnd/team7energy/src/Components/HomePage.jsx
@@ -13,32 +13,13 @@ const HomePage = () => {
       navigate("/");
     }
   }, [isLogout]);
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
     <div>
       <TopBar />
       <Container fluid>
         <Row>
-          <Col>
-            <Button variant="primary" onClick={handleShow} className="">
-              User info
-            </Button>
-            <Offcanvas show={show} onHide={handleClose}>
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title>
-                  <LeftSideBar />
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images,
-                lists, etc.
-              </Offcanvas.Body>
-            </Offcanvas>
-          </Col>
+          <Col></Col>
         </Row>
       </Container>
     </div>
