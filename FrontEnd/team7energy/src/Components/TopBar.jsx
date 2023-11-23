0c33b01aeea7c9337367c6aaf48e3a2cde01a01c
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-11-23 15:16:10 +0100

    Update TopBar.jsx

--- FrontEnd/team7energy/src/Components/TopBar.jsx
+++ FrontEnd/team7energy/src/Components/TopBar.jsx
@@ -1,18 +1,1 @@
-import { Button, Container, Form, Nav, Navbar, Offcanvas } from "react-bootstrap";
-import { useDispatch } from "react-redux";
 
-import { LOGOUT } from "../Redux/Actions/HomePageActions";
-import LeftSideBar from "./homepageSubComponents/LeftSideBar";
-import { useState } from "react";
-import { ChevronDoubleRight } from "react-bootstrap-icons";
-import CanvasUserDetails from "./homepageSubComponents/CanvasUserDetail";
-
-const TopBar = () => {
-  const dispatch = useDispatch();
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  return <Navbar expand="lg" className="bg-body-tertiary"></Navbar>;
-};
-export default TopBar;
